50c5b91cb370705036a4c492f50d198c86d378df
Mikhail Tatarkov <tmu87@mail.ru>
2022-07-10 15:15:51 +0300

    Place near navigation button in mobile youtube

--- dist/content.js
+++ dist/content.js
@@ -23,6 +23,12 @@ async function getApp() {
       return app;
     }
 
+    app = document.querySelector('iframe#player');
+
+    if (app !== null) {
+      return app;
+    }
+
     await wait(100);
   }
 
@@ -49,47 +55,58 @@ function getControlsContainer() {
 }
 
 function getPlayButtonContainer(controlsContainer) {
+  // for (let i = 0; i < 20; i++) {
   let playButtonContainer = document.querySelector('.ytp-play-button');
 
   if (playButtonContainer === null) {
     playButtonContainer = controlsContainer.querySelector('.default__button');
   }
 
-  if (!playButtonContainer) {
-    playButtonContainer = document.querySelector('.custom_container');
-
-    if (!playButtonContainer) {
-      let container = document.createElement('div');
-      container.style = `
-      background-color: black;
-      z-index: 3;
-      position: relative;
-      padding-top: 10px;
-  `;
-      container.innerHTML = "<div class='custom_container'></div>";
-
-      document.querySelector('#player-control-container').insertAdjacentElement('afterend', container);
-      return document.querySelector('.custom_container');
-    }
-    else {
-      return playButtonContainer;
-    }
+  if (playButtonContainer === null) {
+    playButtonContainer = document.querySelector('.playlist-controls-primary');
+    return playButtonContainer;
   }
+
+  //   await wait(100);
+  // }
+
+  // if (!playButtonContainer) {
+  //   playButtonContainer = document.querySelector('.custom_container');
+
+  //   if (!playButtonContainer) {
+  //     let container = document.createElement('div');
+  //     container.style = `
+  //     background-color: black;
+  //     z-index: 3;
+  //     position: relative;
+  //     padding-top: 10px;
+  // `;
+  //     container.innerHTML = "<div class='custom_container'></div>";
+
+  //     document.querySelector('#player-control-container').insertAdjacentElement('afterend', container);
+  //     return document.querySelector('.custom_container');
+  //   }
+  //   else {
+  //     return playButtonContainer;
+  //   }
+  //}
 
   while (playButtonContainer != controlsContainer && playButtonContainer.parentElement !== controlsContainer) {
     playButtonContainer = playButtonContainer.parentElement;
   }
   return playButtonContainer;
 }
+
 function initApp(app) {
   {
 
     function createNextNSecondButton(n) {
 
       let controlsContainer = getControlsContainer();
+      let playButtonContainer = getPlayButtonContainer(controlsContainer);
       let name = ('next-' + n + '-seconds').replaceAll('.', '-');
 
-      let next = controlsContainer.querySelector('#' + name);
+      let next = playButtonContainer.querySelector('#' + name);
       if (next) {
         return next;
       }
@@ -97,10 +114,7 @@ function initApp(app) {
       next = document.createElement('button');
       next.id = name;
       next.className = 'ytp-button btn-icon default__button';
-      next.style.cssText = 'display: inline-flex;'
-        + 'justify-content: center;'
-        + 'align-items: center;'
-        + 'vertical-align: top';
+      next.style.cssText = `padding: 0.075rem;display: inline-flex;justify-content: center;align-items: center;`;
 
       next.innerHTML =
         `<svg style="width: auto;height: 65%;" viewBox="0 0 24 24" width="24"><g transform="matrix(1 0 0 1 39.5 10.67)" style=""  >
@@ -112,7 +126,7 @@ function initApp(app) {
       const spanWrapper = document.createElement('span');
       spanWrapper.appendChild(next);
 
-      let playButtonContainer = getPlayButtonContainer(controlsContainer);
+      
       playButtonContainer.insertAdjacentElement('afterend', spanWrapper);
       return next;
     }
@@ -144,19 +158,21 @@ function initApp(app) {
 
       let seconds = [-5, -1, -0.042, 0.042, 1, 5].reverse();
 
+
       seconds.forEach(second => {
         const next1SecondButton = createNextNSecondButton(second);
 
         next1SecondButton.onclick = () => {
           moveToSecond(second)
         };
-      })
+      });
 
       let controlsContainer = getControlsContainer();
+      let playButtonContainer = getPlayButtonContainer(controlsContainer);
 
       let name = ('speedSelect');
 
-      let speedCtrl = controlsContainer.querySelector('#' + name);
+      let speedCtrl = playButtonContainer.querySelector('#' + name);
       if (speedCtrl) {
         return speedCtrl;
       }
@@ -164,16 +180,25 @@ function initApp(app) {
       speedCtrl = document.createElement('select');
       speedCtrl.id = name;
       // next.className = 'ytp-button btn-icon default__button';
-      speedCtrl.style.cssText = 'display: inline-flex;'
-        + 'justify-content: center;'
-        + 'align-items: center;'
-        + 'font-size: 14px;'
-        + 'vertical-align: top';
+      speedCtrl.style.cssText = `
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 14px;
+      vertical-align: top;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      color: white;
+      background-color: black;
+      border: 0;
+      margin-right: 5px;
+      margin-left: 5px;
+      `;
 
-      speedCtrl.innerHTML =
-        `<option >2</option><option >1.5</option><option >1.1</option><option selected="selected">1</option><option >0.9</option><option >0.75</option><option >0.5</option>`;
+      let speeds = [2, 1.5, 1.1, 1, 0.9, 0.75, 0.5];
 
-      let playButtonContainer = getPlayButtonContainer(controlsContainer);
+      speedCtrl.innerHTML = speeds.map(speed => ('<option value="' + speed + '" ' + (speed === 1 ? 'selected="selected"' : '') + '>' + speed + 'X</option>')).join('');
 
       playButtonContainer.insertAdjacentElement('afterend', speedCtrl);
 
@@ -185,6 +210,45 @@ function initApp(app) {
       }
       speedCtrl.onchange = playbackRateChange;
 
+      document.querySelector('.playback-rate').style = "display:none";
+      //       let timeMarks = document.createElement('select');
+      //       timeMarks.id = "timeMarks";
+      //       // next.className = 'ytp-button btn-icon default__button';
+      //       timeMarks.style.cssText = 'display: inline-flex;'
+      //         + 'justify-content: center;'
+      //         + 'align-items: center;'
+      //         + 'font-size: 14px;'
+      //         + 'vertical-align: top';
+
+      //         timeMarks.innerHTML =
+      // `<option value="" data-v-469af010="">(none)</option><option value="20.123354" data-v-469af010="">Ганчо с теневой</option>`
+
+      // let span=document.createElement('span');
+      // span.innerHTML=`<span v-if="showTimeMarkName">
+      // [{{ getCurrentTimeStr() }}] Mark name
+      // <input v-model="timeMarkName" />
+      // <img
+      //   class="add ctrl-img"
+      //   src="/src/assets/check-square.svg"
+      //   @click="addTime"
+      // />
+      // </span>
+
+      // <img
+      // v-if="!showTimeMarkName"
+      // class="add ctrl-img"
+      // src="/src/assets/plus-square.svg"
+      // @click="toAddTime"
+      // />
+      // <img
+      // v-if="!showTimeMarkName && moment"
+      // class="add ctrl-img"
+      // src="/src/assets/trash-2.svg"
+      // @click="removeTime"
+      // />`;
+
+      // playButtonContainer.insertAdjacentElement('afterend', timeMarks);
+      // playButtonContainer.insertAdjacentElement('afterend', span);
       document.onkeydown = (e) => {
 
         if (e.ctrlKey) {
@@ -258,25 +322,30 @@ getApp().then(app => {
   if (app) {
     initApp(app);
   }
-  else {
-    let next = document.createElement('button');
-    next.id = "initLater";
-   // next.className = 'ytp-button btn-icon default__button';
-    next.style.cssText = 'position: absolute;z-index: 1000;bottom: 0px;right: 0px;color: red;border-color: black;';
+  // else
+  {
+    //     let next = document.createElement('button');
+    //     next.id = "initLater";
+    //    // next.className = 'ytp-button btn-icon default__button';
+    //     next.style.cssText = 'position: absolute;z-index: 1000;bottom: 0px;right: 0px;color: red;border-color: black;';
 
-    next.innerHTML =
-      `<svg style="width: auto;height: 65%;" viewBox="0 0 24 24" width="24"><g transform="matrix(1 0 0 1 39.5 10.67)" style=""  >
-    <text xml:space="preserve" font-family="'Open Sans', sans-serif" font-size="18" font-style="normal" font-weight="normal"
-     style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4;
-      fill: #ffffff; fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-40" y="5.65" >`+ 111 + `</tspan></text>
-</g></svg>`;
+    //     next.innerHTML =
+    //       `<svg style="width: auto;height: 65%;" viewBox="0 0 24 24" width="24"><g transform="matrix(1 0 0 1 39.5 10.67)" style=""  >
+    //     <text xml:space="preserve" font-family="'Open Sans', sans-serif" font-size="18" font-style="normal" font-weight="normal"
+    //      style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4;
+    //       fill: #ffffff; fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-40" y="5.65" >`+ 111 + `</tspan></text>
+    // </g></svg>`;
 
-    document.body.appendChild(next);
-    next.onclick = () => {
-      getApp().then(app => {
-        initApp(app);
-      });
-    }
+    //     document.body.appendChild(next);
+    //     next.onclick = () => {
+
+    document.body.addEventListener('touchstart', (e) => {
+      if (e.touches.length > 1) {
+        getApp().then(app => {
+          initApp(app);
+        });
+      }
+    });
   }
 });
 
